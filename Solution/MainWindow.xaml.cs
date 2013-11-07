cb9e41e595e8ed6c5b715d83df9b969c74eaaf05
Alexey Matveev <alexey_matveev@epam.com>
2013-11-07 14:21:13 +0400

    Libs added and simple demo implemented

--- Solution/MainWindow.xaml.cs
+++ Solution/MainWindow.xaml.cs
@@ -11,6 +11,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.IO.Packaging;
+using System.Windows.Xps.Packaging;
+using System.Windows.Xps;
+using System.Windows.Xps.Serialization;
 
 namespace Solution
 {
@@ -22,6 +27,28 @@ namespace Solution
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            using (var stream = new FileStream("doc.xps", FileMode.Create))
+            {
+                using (var package = Package.Open(stream, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    using (var xpsDoc = new XpsDocument(package, CompressionOption.Maximum))
+                    {
+                        var rsm = new XpsSerializationManager(new XpsPackagingPolicy(xpsDoc), false);
+                        var paginator = DocViewer.Document.DocumentPaginator;
+                        rsm.SaveAsXaml(paginator);
+                        rsm.Commit();
+                    }
+                }
+                stream.Position = 0;
+            
+                var pdfXpsDoc = PdfSharp.Xps.XpsModel.XpsDocument.Open(stream);
+                PdfSharp.Xps.XpsConverter.Convert(pdfXpsDoc, "doc.pdf", 0);
+            }
+            
         }
     }
 }
